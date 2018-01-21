d3605f8757e378673f73a2211013dabd68fc7f97
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { reactReduxFirebase, firebaseStateReducer } from 'react-redux-firebase';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import firebase from 'firebase';
 
 import App from './App';
@@ -21,13 +23,18 @@ firebase.initializeApp(firebaseConfig);
 const store = createStore(
     combineReducers({
         firebase: firebaseStateReducer,
+        routing: routerReducer,
     }),
     reactReduxFirebase(firebase),
 );
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <Route path='/' component={App} />
+        </Router>
     </Provider>,
     document.getElementById('root'),
 );
