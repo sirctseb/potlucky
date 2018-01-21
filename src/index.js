3e8015f47163e06885fc3b5e42764a1388f69cee
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
+const store = createStore(state => state);
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root'),
 );
