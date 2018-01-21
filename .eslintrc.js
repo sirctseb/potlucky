184bec35dfc5cbb16f1de03ad829dd85abbb239e
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import",
+        "react",
+        "babel"
+    ],
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "browser": true
+    }
+};
