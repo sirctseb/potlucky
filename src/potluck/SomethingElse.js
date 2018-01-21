fee84b926fbed1760cb080ad2ea79ab8ed09239f
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:43 -0500

    rearrange and style SomethingElse

--- src/potluck/SomethingElse.js
+++ src/potluck/SomethingElse.js
@@ -11,22 +11,23 @@ class SomethingElse extends Component {
                     this.props.onSubmit(values);
                     this.props.reset();
                 })}>
-                <Field name='bringer'
-                    component={InconspicuousInput}
-                    input={{
-                        placeholder: 'Who are you?',
-                    }} />
-                <Field name='name'
-                    component={InconspicuousInput}
-                    input={{
-                        placeholder: 'What are you bringing?',
-                    }}/>
-                <div type='submit'
-                    onClick={this.props.handleSubmit((values) => {
-                        this.props.onSubmit(values);
-                        this.props.reset();
-                    })}>
-                    bring this
+                <h4 className='something-else__title'>
+                    or bring something else:
+                </h4>
+                <div className='something-else__inputs'>
+                    <Field name='bringer'
+                        component={InconspicuousInput}
+                        placeholder='Who are you?' />
+                    <Field name='name'
+                        component={InconspicuousInput}
+                        placeholder='What are you bringing?' />
+                    <div className='something-else__submit'
+                        onClick={this.props.handleSubmit((values) => {
+                            this.props.onSubmit(values);
+                            this.props.reset();
+                        })}>
+                        bring it!
+                    </div>
                 </div>
             </form>
         );
