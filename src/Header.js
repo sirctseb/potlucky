707946225169e076d3b05b93858d8bb9ddb9c673
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    start branding colors and title styling

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,7 @@ class Header extends Component {
     render() {
         return (
             <header className='header'>
+                <h1 className='title'>Potlucky</h1>
                 <button className='header__create-potluck'
                     onClick={() => {
                         this.props.firebase.push('potlucks', emptyPotluck)
