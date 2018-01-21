ea9ddfaa7177ad2dec08b326f2036a43f7cc729a
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:43 -0500

    put attribution contents on one line to maintain whitespace

--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,7 @@ class Footer extends Component {
         return (
             <div className='footer'>
                 <div className='footer__attribution'>
-                    Icons made by
-                    <a href="http://www.freepik.com" title="Freepik">Freepik</a>
-                    from
-                    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                    is licensed by
-                    <a href="http://creativecommons.org/licenses/by/3.0/"
-                        title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+                    Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
                 </div>
             </div>
         );
