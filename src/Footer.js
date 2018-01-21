e2d65b51dca3012e3cf365606f37ea50ce3ca83a
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:43 -0500

    add authorship and vertically center footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,9 @@ class Footer extends Component {
     render() {
         return (
             <div className='footer'>
+                <div className='footer__signature'>
+                    by <a href='https://twitter.com/sirctseb'>@sirctseb</a>
+                </div>
                 <div className='footer__attribution'>
                     Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
                 </div>
