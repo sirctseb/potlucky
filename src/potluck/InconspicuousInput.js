178794a96ad64f027e4b946f56dbcca6f1d332e0
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:43 -0500

    explicitly pass placeholder through inconspicuous input

--- src/potluck/InconspicuousInput.js
+++ src/potluck/InconspicuousInput.js
@@ -5,6 +5,7 @@ class InconspicuousInput extends Component {
     render() {
         return (
             <input {...(this.props.input || this.props)}
+                placeholder={this.props.placeholder}
                 type='text'
                 className={`inconspicuous-input ${get(this.props, 'className', '')}`} />
         );
