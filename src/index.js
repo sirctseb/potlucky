b951bea147bc7038770e3ebeb1054792a7e013ca
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { reactReduxFirebase, firebaseStateReducer } from 'react-redux-firebase';
 
 import App from './App';
 
-const store = createStore(state => state);
+const firebaseConfig = {
+    apiKey: 'AIzaSyCtUN0dVdhGZuG6TyrzGJWas4jsQI5ujas',
+    authDomain: 'potlucky-dev.firebaseapp.com',
+    databaseURL: 'https://potlucky-dev.firebaseio.com',
+    projectId: 'potlucky-dev',
+    storageBucket: 'potlucky-dev.appspot.com',
+    messagingSenderId: '83393501900',
+};
+
+const store = createStore(
+    combineReducers({
+        firebase: firebaseStateReducer,
+    }),
+    reactReduxFirebase(firebaseConfig),
+);
 
 ReactDOM.render(
     <Provider store={store}>
