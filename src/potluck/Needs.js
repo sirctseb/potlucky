332775efe3ada5b89ab2696348a34aaab15b1671
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:42 -0500

    add bring and delete buttons to needs

--- src/potluck/Needs.js
+++ src/potluck/Needs.js
@@ -4,7 +4,7 @@ import { get, map } from 'lodash';
 class Needs extends Component {
     render() {
         const path = `potlucks/${this.props.params.potluckId}`;
-        const { update } = this.props.firebase;
+        const { update, remove } = this.props.firebase;
         return (
             <div className='needs'>
                 <div className='needs__title'>What we need</div>
@@ -12,18 +12,29 @@ class Needs extends Component {
                     map(get(this.props.potluck, 'needs'), (value, key) =>
                         !value.brought &&
                         <div key={key}
-                            className='needs__need'
-                            onClick={() => {
-                                update(path, {
-                                    [`needs/${key}/brought`]: true,
-                                    [`bringings/${key}`]: {
-                                        bringer: '',
-                                        neededKey: key,
-                                        name: value.name,
-                                    },
-                                });
-                            }}>
+                            className='needs__need'>
                             {value.name}
+                            <div className='needs_need-bring'
+                                title={'I\'ll bring this!'}
+                                onClick={() => {
+                                    update(path, {
+                                        [`needs/${key}/brought`]: true,
+                                        [`bringings/${key}`]: {
+                                            bringer: '',
+                                            neededKey: key,
+                                            name: value.name,
+                                        },
+                                    });
+                                }}>
+                                o
+                            </div>
+                            <div className='needs_need-delete'
+                                title={'Nevermind, we don\'t need this'}
+                                onClick={() => {
+                                    remove(`${path}/needs/${key}`);
+                                }}>
+                                x
+                            </div>
                         </div>)
                 }
             </div>
