87d620118224d3ecbac19d88763e91dfd8ed95b3
Christopher Best <bestchris@gmail.com>
2018-01-21 15:22:43 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
     render() {
         return (
             <div className='app'>
                 <Header />
-                { this.props.children }
+                <div className='app__body'>
+                    { this.props.children }
+                </div>
+                <Footer />
             </div>
         );
     }
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className='footer'>
+                <div className='footer__attribution'>
+                    Icons made by
+                    <a href="http://www.freepik.com" title="Freepik">Freepik</a>
+                    from
+                    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                    is licensed by
+                    <a href="http://creativecommons.org/licenses/by/3.0/"
+                        title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
